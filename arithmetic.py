0b8940c3de692d58072b531d4820508ac5b25d1b
sukriti-waani <sukritiwaani2006@gmail.com>
2025-09-14 21:37:57 +0530

    Add NumPy array operations: addition, multiplication, division, and modulo

---
+++ arithmetic.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+# Add 3 to each element
+var = np.array([1, 2, 3, 4])
+varadd = var + 3
+print(varadd)
+
+print("\n")
+
+# Add two arrays element-wise
+var1 = np.array([1, 2, 3, 4])
+var2 = np.array([1, 2, 3, 4])
+varadd1 = var1 + var2
+print(varadd1)
+
+# Multiply each element by 3
+var = np.array([1, 2, 3, 4])
+varadd2 = var * 3
+print(varadd2)
+
+print("\n")
+
+# Divide each element by 3
+var2 = np.array([1, 2, 3, 4])
+varadd3 = var2 / 3
+print(varadd3)
+
+print("\n")
+
+var2 = np.array([1, 2, 3, 4])
+varadd3 = var2 % 3
+print(varadd3)
