b545764051b61dfe1e9a95a7287dc5ad460e8169
sukriti-waani <sukritiwaani2006@gmail.com>
2025-09-14 21:42:57 +0530

    Use np.add for element-wise addition in NumPy examples

--- arithmetic.py
+++ arithmetic.py
@@ -2,7 +2,8 @@ import numpy as np
 
 # Add 3 to each element
 var = np.array([1, 2, 3, 4])
-varadd = var + 3
+# varadd = var + 3
+varadd = np.add(var,3)
 print(varadd)
 
 print("\n")
@@ -10,7 +11,8 @@ print("\n")
 # Add two arrays element-wise
 var1 = np.array([1, 2, 3, 4])
 var2 = np.array([1, 2, 3, 4])
-varadd1 = var1 + var2
+# varadd1 = var1 + var2
+varadd1 = np.add(var1, var2)
 print(varadd1)
 
 # Multiply each element by 3
